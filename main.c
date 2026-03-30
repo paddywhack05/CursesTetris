8b91daaf0668d1ff1d42994b4daf97e7fc674436
paddywhack05 <100291163+paddywhack05@users.noreply.github.com>
2026-03-30 17:28:14 +0100

    added rotation

--- main.c
+++ main.c
@@ -16,6 +16,8 @@ int rotState = 0;
 int currentBlock = 0;
 int nextBlock = 0;
 int color=2;
+int score=0;
+int totalLinesCleared=0;
 char *UTF;
 typedef struct Cord
 {
@@ -28,6 +30,7 @@ enum Colors{
     Green,
     Blue,
     Yellow,
+    Magenta,
 };
 enum Blocks{
     Block = 1,
@@ -38,8 +41,17 @@ enum Blocks{
     J,
     T,
 };
-
+enum Direction{
+    Right,
+    Left,
+};
 int spawnBlock(int **GameState,Cord *CordArray,int num);
+void incrementRot(){
+    (rotState < 3) ? (rotState++) : (rotState=0);
+}
+void decrementRot(){
+    (rotState==0) ? (rotState=3) : (rotState--);
+}
 void resetGameState(int **GameState){
     int i , j;
 for (i = 0; i < cols; i++) {
@@ -62,7 +74,23 @@ long uniTime(){
     long milli = t.tv_nsec/1e+6;
     return millisec + milli;
 }
-
+void printCords(Cord *CordArray,int pad){
+    move(cols+pad,0);
+    for(int i=0;i<4;i++){
+        printw("x=%d,y=%d, ",CordArray[i].x,CordArray[i].y);
+    }
+}
+void printFixed(int arr[4][4])
+{
+    move(cols+5,0);
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+            printw("%d ", arr[i][j]);
+        printf("\n");
+    }
+    refresh();
+}
 void printGameState(WINDOW *pTetrisWin,int **GameState){
     werase(pTetrisWin);
     char *blockChar = "██";
@@ -179,6 +207,227 @@ int setGround(int **GameState,Cord *CordArray){
    int block = spawnBlock(GameState,CordArray,0);
     return block;
 }
+int applyPadding(Cord *newCordArray,Cord *CordArray,int **GameState){
+        printCords(newCordArray,3);
+            int padding=0;
+            int paddingY=0;
+            if(currentBlock!=Line){
+                padding=0;
+                paddingY =0;
+                padding = (rotState==0) ? 0 : padding;
+                padding = (rotState==1) ? -1 : padding;
+                paddingY = (rotState ==2) ? -1 : paddingY;
+            }else{
+                padding = 0;
+                paddingY =0;
+            }
+            mvprintw(cols+2,0,"Apply the padding");
+            refresh();
+        for(int i=0; i<4;i++){
+        if(newCordArray[i].y+paddingY>=cols||newCordArray[i].x+padding>=rows){
+            mvprintw(cols+2,0,"too far up/right");
+            return -1;
+        }
+        if(newCordArray[i].x+padding<0||newCordArray[i].y+paddingY<0){
+            mvprintw(cols+2,0,"too far Left");
+            //printf("too far left");
+            return -1;
+        }
+        if(GameState[newCordArray[i].y+paddingY][newCordArray[i].x+padding]>=2){
+            mvprintw(cols+2,0,"Collision");
+            return -1;
+        }
+    }
+    for(int i=0; i<4;i++){
+        newCordArray[i].x = newCordArray[i].x+padding;
+        newCordArray[i].y = newCordArray[i].y+paddingY;
+    }
+    //mvprintw(cols+2,0,"Success \n%d",newCordArray[0].y);
+    printCords(newCordArray,4);
+    refresh();
+    return 0;
+}
+void rotateLine(int **GameState,Cord *CordArray,int direction){
+        int box[4][4]={0};
+        int paddingY;
+        int paddingX;
+
+        paddingX = (rotState==0) ? CordArray[0].x : paddingX;
+        paddingY = (rotState==0) ? CordArray[0].y-1 : paddingY;
+
+        paddingX = (rotState==1) ? CordArray[0].x-2 : paddingX;
+        paddingY = (rotState==1) ? CordArray[0].y : paddingY;
+
+        paddingX = (rotState==2) ? CordArray[0].x : paddingX;
+        paddingY = (rotState==2) ? CordArray[0].y-2 : paddingY;
+
+        paddingX = (rotState==3) ? CordArray[0].x-1 : paddingX;
+        paddingY = (rotState==3) ? CordArray[0].y : paddingY;
+
+        for(int i=0;i<4;i++){
+           int Y = CordArray[i].y-paddingY;
+           int X = CordArray[i].x-paddingX;
+           box[Y][X]=1;
+        }
+        //Transpose
+        int box2[4][4]={0};
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                box2[i][j] = box[j][i];
+                //box[i][j] = 0;
+            }
+        }
+        //Reverse
+        if(direction == Right){
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                box[i][3-j] = box2[i][j];
+            }
+        }
+        }else if(direction == Left){
+            for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                box[3-i][j] = box2[i][j];
+            }
+        }
+        }
+            int num=0;
+            Cord CordArray_rotated[4];
+            printFixed(box);
+        for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+        if(box[i][j]==1){
+            CordArray_rotated[num].y=i+paddingY;
+            CordArray_rotated[num].x=j+paddingX;
+            num++;
+            }
+            }
+        }
+        int code = applyPadding(CordArray_rotated,CordArray,GameState);
+        if(code!=ERR){
+            appendCordArray(GameState,CordArray,CordArray_rotated);
+            memcpy(CordArray,CordArray_rotated,sizeof(Cord)*4);
+            if(direction == 0){
+                incrementRot();
+            }else{
+                decrementRot();
+            }
+        }
+}
+void rotate(int **GameState,Cord *CordArray,int padX,int padY,int direction){
+            int matrix[3][3]={0};
+
+        //Loop through x cords set lowest to var
+        int Xnum = CordArray[0].x;
+        for(int i=1;i<4;i++){
+            if(CordArray[i].x<Xnum){
+            Xnum = CordArray[i].x;
+            }else{
+                continue;
+            }
+        }
+        int Ynum = CordArray[0].y;
+        for(int i=1;i<4;i++){
+            if(CordArray[i].y<Ynum){
+            Ynum = CordArray[i].y;
+            }else{
+                continue;
+            }
+        }
+        for(int i=0;i<4;i++){
+           int Y = CordArray[i].y-Ynum+padY;
+           int X = CordArray[i].x-Xnum+padX;
+           matrix[Y][X]=1;
+        }
+        //Transpose
+        int matrix2[3][3]={0};
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                matrix2[i][j] = matrix[j][i];
+            }
+        }
+        //Reverse
+
+            for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+               // matrix[i][j] = matrix2[2-i][2-j];
+               matrix[i][j] = matrix2[i][j];
+            }
+        }
+        if(direction==Left){
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                matrix[i][j] = matrix2[2-i][j];
+            }
+        }
+        }else if(direction==Right){
+            for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                matrix[i][j] = matrix2[i][2-j];
+            }
+        }
+        }
+            int index=0;
+            Cord CordArray_rotated[4];
+        for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+        if(matrix[i][j]==1){
+            CordArray_rotated[index].y=i+Ynum;
+            CordArray_rotated[index].x=j+Xnum;
+            index++;
+            }
+            }
+        
+        }
+        int code = applyPadding(CordArray_rotated,CordArray,GameState);
+        if(code!=ERR){
+            appendCordArray(GameState,CordArray,CordArray_rotated);
+            memcpy(CordArray,CordArray_rotated,sizeof(Cord)*4);
+            if(direction == 0){
+                incrementRot();
+            }else{
+                decrementRot();
+            }
+        }
+}
+
+void rotateRight(int **GameState,Cord *CordArray){
+        int padY=0;
+        int padX=0;
+        padX = (rotState == 1) ? 1 : padX;
+        padY = (rotState == 2) ? 1 : padY;
+    switch (currentBlock)
+    {
+       case Block://block
+        return;
+        break;
+        case Line://line
+           rotateLine(GameState,CordArray,Right);
+        break;
+        default:
+            rotate(GameState,CordArray,padX,padY,Right);
+        break;
+    }
+}
+void rotateLeft(int **GameState,Cord *CordArray){
+        int padY=0;
+        int padX=0;
+        padX = (rotState == 1) ? 1 : padX;
+        padY = (rotState == 2) ? 1 : padY;
+    switch (currentBlock)
+    {
+       case Block://block
+        return;
+        break;
+        case Line://line
+           rotateLine(GameState,CordArray,Left);
+        break;
+        default:
+            rotate(GameState,CordArray,padX,padY,Left);
+        break;
+    }
+}
 
 void moveRight(int **GameState,Cord *CordArray){
     for(int i=3; i>=0;i--){
@@ -253,6 +502,7 @@ int main(){
     init_pair(Green, COLOR_GREEN, COLOR_BLACK);
     init_pair(Blue, COLOR_BLUE, COLOR_BLACK);
     init_pair(Yellow, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(Magenta, COLOR_MAGENTA, COLOR_BLACK);
     //init_pair(3, COLOR_BLACK, COLOR_GREEN);
     printw("how mant rows\n");
     scanw(" %d",&rows);
@@ -322,8 +572,14 @@ int main(){
             }
             break;
             case 'w':
-            break;
+
             case KEY_UP:
+
+            case 'e':
+            rotateRight(GameState,CordArray);
+            break;
+            case 'q':
+            rotateLeft(GameState,CordArray);
             break;
             case 27:
             goto GameOver;
@@ -367,8 +623,9 @@ int spawnBlock(int **GameState,Cord *CordArray,int block){
         rNum = block;
     }
 
-    //rNum=Block;//!REMEMBER ME
-    color=(rand() % (4-1+1))+2;
+    //rNum=Line;//!REMEMBER ME
+    currentBlock = rNum;
+    color=(rand() % (5-1+1))+2;
     Cord oldCordArray[4];
     memcpy(oldCordArray,CordArray,sizeof(Cord)*4);
     switch (rNum)
